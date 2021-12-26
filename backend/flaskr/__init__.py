82c4f80feccc8214bce4d112ec7779b774bec9de
wbtaro <m.ryotaro.325@gmail.com>
2021-12-26 21:29:28 +0900

    Add delete question function

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import random
 
-from models import setup_db, Question, Category
+from models import setup_db, db, Question, Category
 
 QUESTIONS_PER_PAGE = 10
 
@@ -93,6 +93,13 @@ def create_app(test_config=None):
   TEST: When you click the trash icon next to a question, the question will be removed.
   This removal will persist in the database and when you refresh the page. 
   '''
+  @app.route('/questions/<int:question_id>', methods=['DELETE'])
+  def delete_question(question_id):
+    db.session.delete(Question.query.get(question_id))
+    db.session.commit()
+    return jsonify({
+      'status': 200
+    })
 
   '''
   @TODO: 
